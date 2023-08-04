2667ad11cd98659ab3bc3ca73fad7ce1d53e42bc
lshep <lori.shepherd@roswellpark.org>
2023-08-04 09:18:16 -0400

    User requested Deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,17 @@
-.onAttach <- function(libname, pkgname){
-    packageStartupMessage("Welcome to the DeepBlueR package")
+#.onAttach <- function(libname, pkgname){
+#    packageStartupMessage("Welcome to the DeepBlueR package")
 
-    if(RCurl::url.exists("http://deepblue.mpi-inf.mpg.de")){
-        packageStartupMessage("DeepBlue is online")
-    }else{
-        warning("DeepBlue could not be reached. Check network connectivity.")
-    }
+#    if(RCurl::url.exists("http://deepblue.mpi-inf.mpg.de")){
+#        packageStartupMessage("DeepBlue is online")
+#    }else{
+#        warning("DeepBlue could not be reached. Check network connectivity.")
+#    }
+#}
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.19")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
+
